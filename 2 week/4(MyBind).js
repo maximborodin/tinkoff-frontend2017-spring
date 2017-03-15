005f80595c8e41127948933ed387b716faf8bb96
maximborodin <borodin.ms@phystech.edu>
2017-03-15 12:20:25 +0300

    Added MyBind Task

---
+++ 2 week/4(MyBind).js
@@ -0,0 +1,14 @@
+"use strict"
+
+function myBind(func, context) {
+  return function() {
+    return func.apply(context, arguments);
+  }
+}
+
+function f() {
+  console.log(this);
+}
+
+var g = myBind(f, 10, 12);
+g();
